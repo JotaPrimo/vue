91a011121561ffa1d51281f42fb666a1d1ac7959
sistemasIMCG <sistemas@institutomirim.org.br>
2021-11-04 19:12:10 -0400

    primeiro componente cirado com sucesso

---
+++ resources/js/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+
+      <div class="container">
+
+         <div class="row mt-3">
+            
+            <div class="col-sm">
+
+                <div class="card">
+                   <div class="card-header bg-white">
+                      I am exemple complonente
+                   </div>
+
+                   <div class="card-body">
+                        I am exemple complonente
+                   </div>
+
+                </div>
+
+            </div>
+
+            </div>
+
+        </div>
+
+</template>
+
+<script>
+    export default {
+     
+    }
+</script>
